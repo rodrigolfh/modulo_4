15f1d764f547767c6e29ea2fef63e942a53d5e10
rodrigolfh <dr.rodrigofuenzalida@gmail.com>
2023-05-09 00:36:30 -0400

    se logra a medias sobrecarga de función teléfonos, pero hay problemas al usar un diccionario para almacenar los números

--- 4_individual/4_individual.py
+++ 4_individual/4_individual.py
@@ -7,28 +7,34 @@ class Persona:
         self._apellido1 = apellido1
         self._apellido2 = apellido2
         self._edad = edad
-        self.fonos = []
-        self.fonos.append(fonos)
-        self.fonos_contacto = {1: '', 2: '', 3: '', 4: ''}
-        for fono in fonos: self.fonos_contacto[fonos.index(fono)] = fono
+       
+        #poner un if para que no sean más de 4 fonos
+        self.fonos = fonos #tupla
+        self._fonos_contacto = {1: '', 2: '', 3: '', 4: ''}
+        for fono in fonos: self._fonos_contacto[fonos.index(fono)+1] = fono
         self.dirección = dirección #no protegido, actualiza constantemente
-    
+        print(self.fonos)
+        print(self._fonos_contacto)
+        
+    def __str__(self):              
+        return self._rut
+        #return(f"RUT : {self._rut} Nombres : {self._nombres} Apellidos{self._apellido1 + self._apellido2}Fecha de Nacimiento : xx/xx/xxxxEdad : {self._edad}Dirección : {self.dirección}Fonos : {self.fonos_contacto}")
+    #sobrecarga
     @property
     def teléfonos(self):
-        for item in self.fonos_contacto:
-            print(f"{self.fonos_contacto.index(item)}: {self.fonos_contacto[item]}")
+        return(self._fonos_contacto)
+    
+        
 
-    @classmethod
+    @teléfonos.setter
     def teléfonos(self, movimiento, index_número=None, número=None):
-        if movimiento == "limpiar": self.fonos_contacto = {1: '', 2: '', 3: '', 4: ''}
-        elif movimiento == "definir": self.fonos_contacto[index_número] = número
-        elif movimiento == "borrar": self.fonos_contacto[index_número] = ''
+        if movimiento == "limpiar":
+            self._fonos_contacto = {1: '', 2: '', 3: '', 4: ''}
+        elif movimiento == "definir": self._fonos_contacto[index_número] = número
+        elif movimiento == "borrar": self._fonos_contacto[index_número] = ''
         else: print("Debe ingresar el movimiento correcto")   
     
     
-    def __str__(self):
-              
-        return f"RUT : {self._rut}\nNombres : {self._nombres}\n Apellidos{self._apellido1 + self._apellido2}\nFecha de Nacimiento : xx/xx/xxxx\nEdad : {self._edad}\nDirección : {self.dirección}\nFonos : {[fono for fono in self.fonos]}" 
 
 class Paciente(Persona):
     def __init__(self, previsión, *programas):
@@ -62,4 +68,5 @@ class Atención:
 
 
 
-paciente1 = Paciente("15695254-0", "Rodrigo Lautaro", "Fuenzalida", "Herrera", "1984/04/04", "Calle La Calle 1", [123456789, 23456789])             
+paciente1 = Paciente("15695254-0", "Rodrigo Lautaro", "Fuenzalida", "Herrera", "1984/04/04", "Calle La Calle 1", 123456789, 23456789)             
+persona1 = Persona("15695254-0", "Rodrigo Lautaro", "Fuenzalida", "Herrera", "1984/04/04", "Calle La Calle 1", 123456789, 23456789)             
